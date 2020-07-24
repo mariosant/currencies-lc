43dad3ff9f2d8b9f0f9d345fb06ae18efbf223e5
Marios Antonoudiou <mariosant@sent.com>
2020-07-24 17:33:48 +0300

    Add accounts support

---
+++ src/store-modules/auth.js
@@ -0,0 +1,23 @@
+import { accountsSdk } from '@livechat/accounts-sdk'
+const { SNOWPACK_PUBLIC_LC_CLIENT_ID } = import.meta.env
+
+const auth = (store) => {
+	const authInstance = accountsSdk.init({
+		client_id: SNOWPACK_PUBLIC_LC_CLIENT_ID,
+		onIdentityFetched: (error, data) => {
+			store.dispatch('auth/set', data)
+		},
+	})
+
+	store.on('@init', () => {
+		return {}
+	})
+
+	store.on('auth/set', (state, auth) => {
+		return {
+			auth,
+		}
+	})
+}
+
+export default auth
