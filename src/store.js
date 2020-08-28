8f8e772f86bc4609fe4a3587e45357baca2a58d2
Marios Antonoudiou <mariosant@sent.com>
2020-08-28 18:37:49 +0000

    Persist cards

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createElement } from 'react'
 import { createStoreon } from 'storeon'
 import { StoreContext } from 'storeon/react'
+import { persistState } from '@storeon/localstorage'
 import card from './store-modules/card.js'
 import currencies from './store-modules/currencies.js'
 import auth from './store-modules/auth.js'
@@ -8,7 +9,7 @@ import logrocket from './store-modules/logrocket.js'
 
 const { SNOWPACK_PUBLIC_PRODUCTION } = import.meta.env
 
-const plugins = [logrocket, auth, card, currencies]
+const plugins = [logrocket, auth, card, currencies, persistState(['card'])]
 
 const store = createStoreon(plugins)
 
